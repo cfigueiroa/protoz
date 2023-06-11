92cc379a7397330179c620e4ddbe8334fd740170
cfigueiroa <cfigueiroa@gmail.com>
2023-06-11 16:32:08 -0300

    feat: first working version

---
+++ src/index.ts
@@ -0,0 +1,103 @@
+class Protoz {
+  static getPrototypeChain(obj: object, times: number = 1): object[] {
+    let prototype = obj;
+    const prototypeChain: object[] = [obj];
+
+    for (let i = 0; i < times; i++) {
+      const nextPrototype = Object.getPrototypeOf(prototype);
+
+      if (nextPrototype === null) {
+        break;
+      }
+
+      prototype = nextPrototype;
+      prototypeChain.push(prototype);
+    }
+
+    return prototypeChain;
+  }
+
+  static findClosestAncestorWithProperty(
+    obj: object,
+    property: string
+  ): object | null {
+    let prototype = Object.getPrototypeOf(obj);
+
+    while (prototype !== null) {
+      if (property in prototype) {
+        return prototype;
+      }
+
+      prototype = Object.getPrototypeOf(prototype);
+    }
+
+    return null; // No ancestor found with the property
+  }
+
+  static hasProperty(obj: object, property: string): boolean {
+    let prototype = obj;
+
+    while (prototype !== null) {
+      if (property in prototype) {
+        return true;
+      }
+
+      prototype = Object.getPrototypeOf(prototype);
+    }
+
+    return false; // Property not found
+  }
+
+  static getAllProperties(obj: object): string[] {
+    const properties: string[] = [];
+
+    let prototype = obj;
+
+    while (prototype !== null) {
+      properties.push(...Object.getOwnPropertyNames(prototype));
+      prototype = Object.getPrototypeOf(prototype);
+    }
+
+    return properties;
+  }
+
+  static isPrototypeOf(obj: object, prototype: object): boolean {
+    let currentPrototype = Object.getPrototypeOf(obj);
+
+    while (currentPrototype !== null) {
+      if (currentPrototype === prototype) {
+        return true;
+      }
+
+      currentPrototype = Object.getPrototypeOf(currentPrototype);
+    }
+
+    return false; // Prototype not found
+  }
+
+  static getPrototypeDepth(obj: object): number {
+    let depth = 0;
+    let prototype = obj;
+
+    while (prototype !== null) {
+      depth++;
+      prototype = Object.getPrototypeOf(prototype);
+    }
+
+    return depth;
+  }
+
+  static getAncestorByDepth(obj: object, depth: number): object | null {
+    let currentDepth = 0;
+    let prototype = obj;
+
+    while (currentDepth < depth && prototype !== null) {
+      currentDepth++;
+      prototype = Object.getPrototypeOf(prototype);
+    }
+
+    return prototype;
+  }
+}
+
+export default Protoz;
---
+++ test/index.test.ts
@@ -0,0 +1,104 @@
+import Protoz from "../src/index";
+
+describe("Protoz", () => {
+  describe("getPrototypeChain", () => {
+    it("should return the prototype chain of an object", () => {
+      const obj = {};
+      const prototypeChain = Protoz.getPrototypeChain(obj);
+
+      expect(prototypeChain).toEqual([obj, Object.getPrototypeOf(obj)]);
+    });
+
+    it("should return the prototype chain with specified number of times", () => {
+      const obj = {};
+      const prototypeChain = Protoz.getPrototypeChain(obj, 2);
+
+      expect(prototypeChain).toEqual([obj, Object.getPrototypeOf(obj)]);
+    });
+  });
+
+  describe("findClosestAncestorWithProperty", () => {
+    it("should find the closest ancestor with a specific property", () => {
+      const obj = {};
+      const ancestor = Protoz.findClosestAncestorWithProperty(obj, "toString");
+
+      expect(ancestor).toBe(Object.prototype);
+    });
+
+    it("should return null if no ancestor has the property", () => {
+      const obj = {};
+      const ancestor = Protoz.findClosestAncestorWithProperty(
+        obj,
+        "nonExistentProperty"
+      );
+
+      expect(ancestor).toBeNull();
+    });
+  });
+
+  describe("hasProperty", () => {
+    it("should return true if the object or its ancestors have the property", () => {
+      const obj = {};
+      const result = Protoz.hasProperty(obj, "toString");
+
+      expect(result).toBe(true);
+    });
+
+    it("should return false if the object and its ancestors do not have the property", () => {
+      const obj = {};
+      const result = Protoz.hasProperty(obj, "nonExistentProperty");
+
+      expect(result).toBe(false);
+    });
+  });
+
+  describe("getAllProperties", () => {
+    it("should return an array of all properties of an object and its ancestors", () => {
+      const obj = {};
+      const properties = Protoz.getAllProperties(obj);
+
+      expect(properties).toContain("toString");
+    });
+  });
+
+  describe("isPrototypeOf", () => {
+    it("should return true if the given prototype is in the object's prototype chain", () => {
+      const obj = {};
+      const result = Protoz.isPrototypeOf(obj, Object.prototype);
+
+      expect(result).toBe(true);
+    });
+
+    it("should return false if the given prototype is not in the object's prototype chain", () => {
+      const obj = {};
+      const result = Protoz.isPrototypeOf(obj, Array.prototype);
+
+      expect(result).toBe(false);
+    });
+  });
+
+  describe("getPrototypeDepth", () => {
+    it("should return the depth of the object's prototype chain", () => {
+      const obj = {};
+      const depth = Protoz.getPrototypeDepth(obj);
+
+      expect(depth).toBe(2);
+    });
+  });
+
+  describe("getAncestorByDepth", () => {
+    it("should return the ancestor at the specified depth", () => {
+      const obj = {};
+      const ancestor = Protoz.getAncestorByDepth(obj, 1);
+
+      expect(ancestor).toBe(Object.prototype);
+    });
+
+    it("should return null if the depth is greater than the prototype chain length", () => {
+      const obj = {};
+      const ancestor = Protoz.getAncestorByDepth(obj, 10);
+
+      expect(ancestor).toBeNull();
+    });
+  });
+});
